54d1ff8f9f0933f586fe30021430e5fdd1056e53
Henrique Castro Da Silva Figueira <46885473820@info.local>
2019-09-02 17:22:41 -0300

    Postman criado

--- BD/02_INCERSAO_BD.sql
+++ BD/02_INCERSAO_BD.sql
@@ -15,8 +15,8 @@ Insert into Estudios values ('Blizzard','Estados Unidos','1991-02-08',1)
 
 select * from Estudios order by EstudioId;
 
-insert into Jogos values ('Diablo 3','é um jogo que contém bastante ação e é viciante','2012-05-15',99,2),
-						('Read Dead Redemption 2','jogo eletrônico de ação-aventura western.','2018-10-26',120,3),
-						('Horizon Chaise Turbo',' Jogo de corrida inspirado em outro jogo de corrida antigo','2016-05-02',45,4);
+insert into Jogos values ('Diablo 3','é um jogo que contém bastante ação e é viciante','2012-05-15',99,1),
+						('Read Dead Redemption 2','jogo eletrônico de ação-aventura western.','2018-10-26',120,2),
+						('Horizon Chaise Turbo',' Jogo de corrida inspirado em outro jogo de corrida antigo','2016-05-02',45,3);
 
 select * from Jogos order by JogoId;
